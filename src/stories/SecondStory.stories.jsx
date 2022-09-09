0a9ccf699204603ac63b64f296c68f6b7c8d5ff0
Chris Schneider <chriskschneider@gmail.com>
2022-09-09 11:56:21 -0400

    second commit

---
+++ src/stories/SecondStory.stories.jsx
@@ -0,0 +1,14 @@
+const SecondStory = ({ text }) => <div>{text}</div>;
+
+export default {
+  title: "SecondStory",
+  component: SecondStory,
+};
+
+// export const Default = () => <Example text="some sample text" />;
+const Template = (args) => <Example {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  text: "some sample text in args object",
+};
